31cb88cf4b89c873bc0ad6343485a1754379c6d5
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2025-07-11 20:48:52 +0300

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [react()],
-  base: "/my-portfolio/", // Your repo name
 });
